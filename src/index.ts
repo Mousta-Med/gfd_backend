20546c136dda740e265dd203801a9e6f6511a5d0
mousata-med <moustamuhammed@gmail.com>
2025-06-02 18:27:03 +0100

    Add JSON parsing and update root response message

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,14 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
+
+app.post("/api/oauth-token", (req: Request, res: Response) => {
+  res.send(req.body);
+});
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+  res.send("Work good");
 });
 
 app.listen(port, () => {
