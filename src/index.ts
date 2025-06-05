bf867f75bd46b7ffe20e95ee351594a250cc8417
Mousta-Med <moustamuhammed@gmail.com>
2025-06-05 17:33:35 +0100

    Implement OAuth token retrieval and update response handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import axios from "axios";
+import { get } from "http";
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -8,8 +10,27 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/api/oauth-token", (req: Request, res: Response) => {
-  res.send(req.body);
+  let { code } = req.body;
+  const token = getAccessToken(code);
+  res.status(200).send("Token received: " + token);
 });
+
+async function getAccessToken(code: string) {
+  const res = await axios.get("https://github.com/login/oauth/access_token", {
+    params: {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code: code,
+      redirect_uri: `http://localhost:3000/integrations/github/oauth2/callback`,
+    },
+    headers: {
+      Accept: "application/json",
+      "Accept-Encoding": "application/json",
+    },
+  });
+
+  return res.data.access_token;
+}
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Works good");
